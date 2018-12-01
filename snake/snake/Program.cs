c1532cb2765a37a5a70b4bada3dfe7c654f685d5
dmakhnev <36293994+dmakhnev@users.noreply.github.com>
2018-12-01 21:21:01 +0300

    работа с классом список

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -15,7 +15,21 @@ namespace snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-            
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pointList = new List<Point>();
+            pointList.Add(p1);
+            pointList.Add(p2);
+
             Console.ReadLine();
         }
         
